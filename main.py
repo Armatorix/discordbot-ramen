d2b8aa121ea979020b7875b929691bf51c1e1772
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2021-12-23 00:07:33 +0100

    sad ramen XD

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import discord
 import os
 import aiohttp
 import io
+from random import randrange, choice
+from serpapi import GoogleSearch
 
+base_image_filename="sad-ramen.jpg"
+ramen_pics={}
+with open(base_image_filename, "rb") as f:
+    ramen_pics[base_image_filename] = f.read()
+
+# search = GoogleSearch({
+#     "q": "ramen",
+#     "api_key": os.getenv('SERAPI_SECRET'),
+#     "tbm": "isch",
+#     "jin": randrange(2137)
+#   })
+# for image_result in search.get_dict()['images_results']:
+#     ramen_pics[""] = image_result["original"]
 
 client = discord.Client()
 
@@ -16,17 +31,21 @@ async def on_message(message):
         return
 
     if any(s in message.content.lower() for s in ['ramen','@r4m3n']):
-        async with aiohttp.ClientSession() as session:
-             async with session.get("https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/tantanmen-ramen-00.jpg") as resp:
-                if resp.status != 200:
-                    return
-                data = io.BytesIO(await resp.read())
-                await message.channel.send(file=discord.File(data, filename="tantanmen-ramen-00.jpg"))
-    if 'bieda' in  message.content or 'biede' in  message.content or 'bogactwo' in  message.content:
+        # async with aiohttp.ClientSession() as session:
+        #      async with session.get("https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/tantanmen-ramen-00.jpg") as resp:
+        #         if resp.status != 200:
+        #             return
+        #         data = io.BytesIO(await resp.read())
+        c= choice(list(ramen_pics))
+        await message.channel.send(file=discord.File(io.BytesIO(ramen_pics[c]), filename=c))
+        return
+
+    if any(s in message.content.lower() for s in ['bieda', 'biede', 'bogactwo']):
         await message.channel.send(
             "JEBAĆ BIEDE! WSZYSCY ZA JEDNEGO! BIEDA BIEDA BIEDA!")
         await message.channel.send(
             '$botify play https://www.youtube.com/watch?v=fQyg-dk_D94'
         )
+        return
 
 client.run(os.getenv('TOKEN'))
